9e90520ca72b51ccb61136115ee13b9d4ce35d0f
AnNet08 <a.e.klyuchko@gmail.com>
2022-05-20 07:42:32 +0300

    Gradient

--- app/src/main/java/com/example/myfigma/MainActivity.kt
+++ app/src/main/java/com/example/myfigma/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.myfigma
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,8 +10,8 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.Green
-import androidx.compose.ui.graphics.Color.Companion.Yellow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myfigma.ui.theme.MyFigmaTheme
@@ -22,13 +21,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyFigmaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
                     ShowCard(Card("Title", "UA 000000000000000", "11 500 500.00", "UA"))
-                }
+
             }
         }
     }
@@ -38,20 +32,36 @@ data class Card(val title: String, val id: String, val balance: String, val curr
 
 @Composable
 fun ShowCard(card: Card) {
-    Column(modifier = Modifier.background(Green)) {
-        Text(text = card.title)
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(text = card.id)
-        Spacer(modifier = Modifier.height(40.dp))
-        Row() {
-            Text(text = card.balance)
-            Spacer(modifier = Modifier.width(4.dp))
-            Text(text = card.currency)
+
+    Box(
+    ) {
+        Surface(
+            shape = MaterialTheme.shapes.medium
+        ) {
+            Column(modifier = Modifier
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            colorResource(R.color.card_bg_start),
+                            colorResource(R.color.card_bg_finish)
+                        )
+                    )
+                )) {
+                Text(text = card.title)
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = card.id)
+                Spacer(modifier = Modifier.height(40.dp))
+                Row() {
+                    Text(text = card.balance)
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(text = card.currency)
+                }
+
+
+            }
         }
 
-
     }
-
 }
 
 @Preview(showBackground = true)
