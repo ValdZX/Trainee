30f20f539c62a18f2e7833029eeba2083e42c548
AnNet08 <a.e.klyuchko@gmail.com>
2022-05-20 10:03:16 +0300

    TempImage

--- app/src/main/java/com/example/myfigma/MainActivity.kt
+++ app/src/main/java/com/example/myfigma/MainActivity.kt
@@ -3,17 +3,26 @@ package com.example.myfigma
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.DefaultAlpha
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myfigma.ui.theme.MyFigmaTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,8 +42,7 @@ data class Card(val title: String, val id: String, val balance: String, val curr
 @Composable
 fun ShowCard(card: Card) {
 
-    Box(
-    ) {
+    Box(contentAlignment = Alignment.Center ) {
         Surface(
             shape = MaterialTheme.shapes.medium
         ) {
@@ -47,7 +55,21 @@ fun ShowCard(card: Card) {
                         )
                     )
                 )) {
-                Text(text = card.title)
+                Row() {
+                    Text(
+                        text = card.title,
+                        modifier = Modifier.padding(all = 16.dp),
+                        style = TextStyle(fontSize = 15.sp)
+                    )
+                    Image(
+                        painter = painterResource(R.drawable.body_clarification_edit),
+                        contentDescription = null,
+                        //alpha = DefaultAlpha,
+                        modifier = Modifier
+                            .size(12.dp)
+                            .padding(all = 16.dp)
+                    )
+                }
                 Spacer(modifier = Modifier.height(4.dp))
                 Text(text = card.id)
                 Spacer(modifier = Modifier.height(40.dp))
