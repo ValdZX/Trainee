857dee172ca2de53f3206e934262a6f90a150288
AnNet08 <a.e.klyuchko@gmail.com>
2022-05-22 22:50:51 +0300

    Constraint

--- app/src/main/java/com/example/myfigma/MainActivity.kt
+++ app/src/main/java/com/example/myfigma/MainActivity.kt
@@ -28,13 +28,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myfigma.ui.theme.MyFigmaTheme
+import androidx.constraintlayout.compose.ConstraintLayout
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyFigmaTheme {
-                ShowCardColumn(Card("Title", "UA 000000000000000", "11 500 500.00", "UA"))
+                ShowCardConstraint(Card("Title", "UA 000000000000000", "11 500 500.00", "UA"))
 
             }
         }
@@ -42,65 +43,6 @@ class MainActivity : ComponentActivity() {
 }
 
 data class Card(val title: String, val id: String, val defaultText: String, val balance: String)
-
-@Composable
-fun ShowCard(card: Card) {
-
-    Box(contentAlignment = Alignment.Center) {
-        Surface(
-            shape = MaterialTheme.shapes.medium
-        ) {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(
-                                colorResource(R.color.card_bg_start),
-                                colorResource(R.color.card_bg_finish)
-                            )
-                        )
-                    )
-                    .padding(16.dp)
-            ) {
-                Row(
-                    modifier = Modifier.padding(all = 16.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = card.title,
-                        //modifier = Modifier.padding(all = 16.dp),
-                        style = TextStyle(fontSize = 15.sp)
-                    )
-                    Spacer(modifier = Modifier.width(10.dp))
-                    Image(
-                        painter = painterResource(R.drawable.edit),
-                        contentDescription = null,
-                        alignment = Alignment.BottomCenter,
-                        modifier = Modifier
-                            .size(12.dp)
-                    )
-                    Spacer(modifier = Modifier.width(203.96.dp))
-                    Image(
-                        painter = painterResource(R.drawable.wallet),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(width = 22.08.dp, height = 18.24.dp)
-                    )
-
-                }
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(text = card.id)
-                Spacer(modifier = Modifier.height(40.dp))
-                Text(text = card.balance)
-                Spacer(modifier = Modifier.width(4.dp))
-
-
-            }
-        }
-
-    }
-}
 
 @Composable
 fun ShowCardBox(card: Card) {
@@ -186,7 +128,8 @@ fun ShowCardColumn(card: Card) {
             .padding(16.dp),
         verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Column(verticalArrangement = Arrangement.Top
+        Column(
+            verticalArrangement = Arrangement.Top
         ) {
             Row(
                 horizontalArrangement = Arrangement.SpaceBetween,
@@ -263,12 +206,98 @@ fun ShowCardColumn(card: Card) {
     }
 }
 
+@Composable
+fun ShowCardConstraint(card: Card) {
+    ConstraintLayout(
+        modifier = Modifier
+            .size(width = 312.dp, height = 184.dp)
+            .clip(RoundedCornerShape(14.dp))
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        colorResource(R.color.card_bg_start),
+                        colorResource(R.color.card_bg_finish)
+                    )
+                )
+            )
+            .padding(16.dp)
+    ) {
+        val (titleText, editImg, walletImg, idText, defaultText, starImg, balanceText) = createRefs()
+
+        Text(
+            text = card.title,
+            modifier = Modifier
+                .padding(start = 0.dp, top = 0.dp, end = 8.dp, bottom = 0.dp)
+                .constrainAs(titleText) {},
+            style = TextStyle(fontSize = 15.sp)
+        )
+        Image(
+            painter = painterResource(R.drawable.edit),
+            contentDescription = null,
+            modifier = Modifier
+                .size(12.dp)
+                .constrainAs(editImg) {
+                    bottom.linkTo(titleText.bottom)
+                    top.linkTo(titleText.top)
+                    start.linkTo(titleText.end)
+                }
+        )
+
+        Image(
+            painter = painterResource(R.drawable.wallet),
+            contentDescription = null,
+            modifier = Modifier
+                .size(width = 24.dp, height = 24.dp)
+                .constrainAs(walletImg) {
+                    end.linkTo(parent.end)
+                }
+        )
+        Text(
+            text = card.id,
+            style = TextStyle(fontSize = 14.sp),
+            modifier = Modifier
+                .constrainAs(defaultText) {
+                    top.linkTo(titleText.bottom)
+                }
+        )
+        Text(
+            text = card.defaultText,
+            style = TextStyle(fontSize = 10.sp),
+            modifier = Modifier
+                .constrainAs(idText) {
+                    bottom.linkTo(starImg.top)
+                }
+                .padding(horizontal = 0.dp, vertical = 8.dp)
+        )
+        Image(
+            painter = painterResource(R.drawable.star),
+            contentDescription = null,
+            modifier = Modifier
+                .size(width = 24.dp, height = 24.dp)
+                .constrainAs(starImg) {
+                    bottom.linkTo(parent.bottom)
+                }
+        )
+        Text(
+            text = card.balance,
+            style = TextStyle(fontSize = 24.sp),
+            modifier = Modifier
+                .constrainAs(balanceText) {
+                    top.linkTo(starImg.top)
+                    end.linkTo(parent.end)
+                }
+        )
+    }
+
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyFigmaTheme {
         //ShowCardBox(Card("Title", "UA 000000000000000", "По умолчанию", "11 500 500.00 UAH"))
-        ShowCardColumn(Card("Title", "UA 000000000000000", "По умолчанию", "11 500 500.00 UAH"))
+        //ShowCardColumn(Card("Title", "UA 000000000000000", "По умолчанию", "11 500 500.00 UAH"))
+        ShowCardConstraint(Card("Title", "UA 000000000000000", "По умолчанию", "11 500 500.00 UAH"))
     }
 }
 
